08bcaae5a4f567f9d9fe67ce2a2298bb6c2f4e2f
HabGeburtstag <f.jonathan.ebeling@gmail.com>
2021-02-19 09:53:57 +0100

    Added login-view without functions yet

--- index.js
+++ index.js
@@ -116,6 +116,10 @@ app.get("/todo", (req, res) => {
   res.render("todo");
 });
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 app.get("/", (req, res) => {
   res.redirect("/todo");
 });
